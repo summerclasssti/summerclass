ac673bea2e9ad61d9a2f7e43ce38b55756c61768
Ronald <ronaldmaymone@id.uff.br>
2022-01-11 15:43:33 -0300

    comentando codigo

--- lib/app/controllers/details_controller.dart
+++ lib/app/controllers/details_controller.dart
@@ -2,14 +2,12 @@ import 'package:get/get.dart';
 
 class DetailsController extends GetxController {
 
+  //Variável observável. Ao atualizá-la os Widgets GetX e ObX que a contém também são atualizados.
   final _likedMovie = false.obs;
   get likedMovie => _likedMovie.value;
   set likedMovie(value) => _likedMovie.value = value;
 
-  void onPreesedBackBtn() {
-    Get.back();
-  }
-
+  //Executado ao pressionar o coração de like.
   void onPressedLikeBtn() {
     likedMovie = !likedMovie;
   }
